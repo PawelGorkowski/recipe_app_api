95c6f244089b9daf34609bf75e02ff8aad9cb2bd
PawelGorkowski <risk0@o2.pl>
2022-06-26 23:38:19 +0200

    sect. 7 finished, tdd with django

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+calc functions
+"""
+
+
+def add(x, y):
+    """add x and y and returns result"""
+    return x + y
+
+
+def substract(x, y):
+    """substract x and y and returns result"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""sample tests"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """test that two numbers are added together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """test that two numbers are substracted"""
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
