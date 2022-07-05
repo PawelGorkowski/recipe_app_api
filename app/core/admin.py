099405a3933bd39d659b86254afafb6f54b5399b
Pawel Gorkowski <risk0@o2.pl>
2022-07-05 23:36:36 +0200

    admin nwq

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 """
 Django admin customization
 """
-from dataclasses import field
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -10,56 +10,32 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-    """Define the admin pages for users"""
+    """Define the admin pages for users."""
 
     ordering = ["id"]
     list_display = ["email", "name"]
     fieldsets = (
-        (
-            (
-                (
-                    None,
-                    {
-                        "fields": (
-                            "email",
-                            "password",
-                        ),
-                    },
-                )
-            ),
-            (
-                _("Permissions"),
-                {
-                    "fields": (
-                        "is_active",
-                        "is_staff",
-                        "is_superuser",
-                    )
-                },
-            ),
-            (
-                _("Important dates"),
-                {"fields": ("last_login",)},
-            ),
-        ),
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("name",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
     )
+
     readonly_fields = ["last_login"]
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "email",
-                    "password1",
-                    "password2",
-                    "name",
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                ),
-            },
-        ),
+        None,
+        {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "name",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            ),
+        },
     )
 
 
