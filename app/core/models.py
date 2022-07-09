a9ccb4a1021a65cb714d4e8a99d1966f9647adc5
Pawel Gorkowski <risk0@o2.pl>
2022-07-10 00:28:35 +0200

    recipe model created

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@
 Database models.
 """
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -45,3 +46,45 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        """Return string representation of the object."""
+        return self.title
+
+    # def get_absolute_url(self):
+    #     """Return URL for recipe detail."""
+    #     return f"/recipe/{self.id}"
+
+    # def get_total_price(self):
+    #     """Return total price of the recipe."""
+    #     return self.price * self.time_minutes
+
+    # def get_total_time(self):
+    #     """Return total time of the recipe."""
+    #     return self.time_minutes
+
+    # def get_ingredients(self):
+    #     """Return list of ingredients."""
+    #     return self.ingredient_set.all()
+
+    # def get_steps(self):
+    #     """Return list of steps."""
+    #     return self.step_set.all()
+
+    # def get_tags(self):
+    #     """Return list of tags."""
+    #     return self.tag_set.all()
