09cef2f0f1b83531294e2a5666cfc25f472bca10
Pawel Gorkowski <risk0@o2.pl>
2022-07-09 20:14:50 +0200

    user api complete + added test_blank_email

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -64,7 +64,13 @@ class PublicUserApiTests(TestCase):
 
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = (
+            get_user_model()
+            .objects.filter(
+                email=payload["email"],
+            )
+            .exists()
+        )
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -103,14 +109,22 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_create_email_blank(self):
-    #     """Test creating a user with a blank email returns an error."""
-    #     payload = {"email": "", "password": "testpass123"}
-    #     res = self.client.post(CREATE_USER_URL, payload)
+        def test_create_email_blank(self):
+            """Test creating a user with a blank email returns an error."""
+            payload = {"email": "", "password": "testpass123"}
+            res = self.client.post(CREATE_USER_URL, payload)
 
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    #     user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
-    #     self.assertFalse(user_exists)
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exists = (
+            get_user_model()
+            .objects.filter(
+                email=payload["email"],
+            )
+            .exists()
+        )
+
+        self.assertFalse(user_exists)
 
     def test_retrieve_user_unauthorized(self):
         """Test authentication is required for users."""
