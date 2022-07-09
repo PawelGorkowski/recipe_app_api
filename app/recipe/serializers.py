e82a3a5dd8e6a52f81d741cf30df668a13a209a4
Pawel Gorkowski <risk0@o2.pl>
2022-07-10 01:50:54 +0200

    linting done right (flake8)

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,13 +12,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = (
-            "id",
-            "title",
-            "time_minutes",
-            "price",
-            "link",
-        )
+        fields = ("id", "title", "time_minutes", "price", "link")
         read_only_fields = ("id",)
 
 
